7c498707ea879e68484078e494f6fe46b4cbb928
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-08-08 20:29:51 -0300

    Update ServicoBiblioteca.java

--- service/ServicoBiblioteca.java
+++ service/ServicoBiblioteca.java
@@ -10,48 +10,57 @@ import java.util.List;
 
 public class ServicoBiblioteca {
 
-	private List<Livro> livros = new ArrayList<>();
-	private List<Membro> membros = new ArrayList<>();
-	private List<Emprestimo> emprestimos = new ArrayList<>();
-	
-	//livros
-	public void adicionarLivro(Livro livro) {
-		livros.add(livro);
-	}
-	
-	public void removerLivro(Livro livro) {
-		livros.remove(livro);
-	}
-	
-	public List<Livro> listarLivros() {
-		return new ArrayList<>(livros);
-	}
-	
-	//membros
-	public void adicionarMembro(Membro membro) {
-		membros.add(membro);
-	}
-	
-	public void removerMembro(Membro membro) {
-		membros.remove(membro);
-	}
-	
-	public List<Membro> listarMembros() {
-		return new ArrayList<>(membros);
-	}
-	
-	//empréstimos
-	public void emprestarLivro(Livro livro, Membro membro) {
-		Emprestimo emprestimo = new Emprestimo(livro, membro, LocalDate.now(), null);
-		emprestimos.add(emprestimo);
-	}
-	
-	public void devolverLivro(Emprestimo emprestimo) {
-		emprestimo.setDataDevolucao(LocalDate.now());
-	}
-	
-	public List<Emprestimo> listarEmprestimos() {
-		return new ArrayList<>(emprestimos);
-	}
-	
+    private List<Livro> livros = new ArrayList<>();
+    private List<Membro> membros = new ArrayList<>();
+    private List<Emprestimo> emprestimos = new ArrayList<>();
+
+    //Livros
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void removerLivro(Livro livro) {
+        livros.remove(livro);
+    }
+
+    public List<Livro> listarLivros() {
+        return new ArrayList<>(livros);
+    }
+
+    //Membros
+    public void adicionarMembro(Membro membro) {
+        membros.add(membro);
+    }
+
+    public void removerMembro(Membro membro) {
+        membros.remove(membro);
+    }
+
+    public List<Membro> listarMembros() {
+        return new ArrayList<>(membros);
+    }
+
+    //Empréstimos
+    public void emprestarLivro(Livro livro, Membro membro) {
+        Emprestimo emprestimo = new Emprestimo(livro, membro, LocalDate.now(), null);
+        emprestimos.add(emprestimo);
+    }
+
+    public void devolverLivro(Emprestimo emprestimo) {
+        emprestimo.setDataDevolucao(LocalDate.now());
+    }
+
+    public List<Emprestimo> listarEmprestimos() {
+        return new ArrayList<>(emprestimos);
+    }
+
+    //verifica se um livro já está emprestado
+    public boolean isLivroEmprestado(Livro livro) {
+        for (Emprestimo emprestimo : emprestimos) {
+            if (emprestimo.getLivro().equals(livro) && emprestimo.getDataDevolucao() == null) {
+                return true; 
+            }
+        }
+        return false;
+    }
 }
